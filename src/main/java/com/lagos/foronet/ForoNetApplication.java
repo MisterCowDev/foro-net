e1bba0138bec731050df98bdc2b9e0f715c659bc
mistercow <esteban.lagos21@gmail.com>
2025-08-15 20:25:12 -0400

    Iniciando proyecto con sus dependencias

---
+++ src/main/java/com/lagos/foronet/ForoNetApplication.java
@@ -0,0 +1,13 @@
+package com.lagos.foronet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForoNetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ForoNetApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lagos/foronet/ForoNetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lagos.foronet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForoNetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
