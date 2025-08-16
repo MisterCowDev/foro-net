bc9cbfdcb1df77c4a0d8a7dee8734fa1d1c3d2b9
mistercow <esteban.lagos21@gmail.com>
2025-08-16 00:32:23 -0400

    CRUT completo en el controlador de topicos

--- src/main/java/com/lagos/foronet/controller/TopicoController.java
+++ src/main/java/com/lagos/foronet/controller/TopicoController.java
@@ -1,22 +1,20 @@
 package com.lagos.foronet.controller;
 
-import com.lagos.foronet.domain.topicos.DatosRegistroTopico;
-import com.lagos.foronet.domain.topicos.Estado;
-import com.lagos.foronet.domain.topicos.Topico;
-import com.lagos.foronet.domain.topicos.TopicoRepository;
+import com.lagos.foronet.domain.topicos.*;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDateTime;
 
 @RestController
 @RequestMapping("/topicos")
-public class ControllerTopico {
+public class TopicoController {
 
     @Autowired
     private TopicoRepository topicoRepository;
@@ -39,5 +37,40 @@ public class ControllerTopico {
 
         topicoRepository.save(topico);
         return ResponseEntity.ok("Tópico registrado correctamente");
+    }
+
+    @GetMapping
+    public ResponseEntity<Page<DatosListaTopicos>> listaTopicos(@PageableDefault(size = 2) Pageable paginacion){
+        return ResponseEntity.ok(topicoRepository.findByEstado(Estado.ACTIVO, paginacion).map(DatosListaTopicos::new));
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<DatosRespuestaTopico> retornaDatosTopicos(@PathVariable Long id){
+        Topico topico = topicoRepository.getReferenceById(id);
+        var datosTopico = new DatosRespuestaTopico(topico.getId(),
+                topico.getTitulo(),
+                topico.getMensaje(),
+                topico.getFechaCreacion(),
+                topico.getAutor(),
+                topico.getCurso());
+        return ResponseEntity.ok(datosTopico);
+
+    }
+
+    @PutMapping
+    @Transactional
+    public ResponseEntity actualizarTopico(@RequestBody @Valid DatosActualizarTopico datosActualizarTopico){
+        Topico topico = topicoRepository.getReferenceById(datosActualizarTopico.id());
+        topico.updateDatos(datosActualizarTopico);
+        return ResponseEntity.ok(new DatosRespuestaTopico(topico.getId(), topico.getTitulo(), topico.getMensaje(),topico.getFechaCreacion(),
+                topico.getAutor(), topico.getCurso()));
+    }
+
+    @DeleteMapping("/{id}")
+    @Transactional
+    public ResponseEntity eliminarTopico(@PathVariable Long id){
+        Topico topico = topicoRepository.getReferenceById(id);
+        topico.desactivarTopico();
+        return ResponseEntity.noContent().build();
     }
 }
