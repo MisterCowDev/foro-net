637e8f508c1880a38c97f1ba7f29934ecae44ae4
mistercow <esteban.lagos21@gmail.com>
2025-08-15 21:37:24 -0400

    Metodo para capturar si el mensaje o titulo se encuentra en la base de datos

--- src/main/java/com/lagos/foronet/domain/TopicoRepository.java
+++ src/main/java/com/lagos/foronet/domain/TopicoRepository.java
@@ -1,4 +1,10 @@
 package com.lagos.foronet.domain;
 
-public interface TopicoRepository {
+import jakarta.validation.constraints.NotBlank;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface TopicoRepository extends JpaRepository<Topico, Long> {
+    Optional<Topico> findByTituloAndMensaje(String titulo, String mensaje);
 }
